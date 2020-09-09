7c91f960e449bf2425b573025aa3e90c3a1e5e4c
JohnHynes <johnthehynes@gmail.com>
2020-09-09 13:20:42 -0400

    Implemented Relationship functions Also included removal of relationships when classes are deleted

--- src/Classes.java
+++ src/Classes.java
@@ -7,6 +7,7 @@ public class Classes {
 	// ********************************************************//
 	// Constructors //
 	// ********************************************************//
+	
 	public Classes() {
 		entities = new ArrayList<Entity>();
 		relationships = new ArrayList<Relationship>();
@@ -15,13 +16,13 @@ public class Classes {
 	// ********************************************************//
 	// Class Functions //
 	// ********************************************************//
+	
 	public boolean createClass(String name) {
-		for (Entity e : entities) {
-			if (e.name.equals(name)) {
-				return false;
-			}
+		Entity e = new Entity(name);
+		if (entities.contains(e)) {
+			return false;
 		}
-		return entities.add(new Entity(name));
+		return entities.add(e);
 	}
 
 	public boolean renameClass(String target, String newname) {
@@ -35,22 +36,31 @@ public class Classes {
 	}
 
 	public boolean deleteClass(String target) {
+		ArrayList<Relationship> temp = new ArrayList<Relationship>();
 		int index = entities.indexOf(new Entity(target));
+		
 		if (index >= 0) {
+			// Removing target
+			entities.remove(index);
+			
+			// Removing all relationships associated with target.
+			// Serial removal in the worst-case is O(RN), while serial adding is O(N-R)
 			for (Relationship r : relationships) {
-				if (r.class1.equals(target) || r.class2.equals(target)) {
-					relationships.remove(r);
+				if (!r.class1.equals(target) && !r.class2.equals(target)) {
+					temp.add(r);
 				}
 			}
-
-			entities.remove(index);
+			relationships = temp;
+			return true;
 		}
+		// Target not found.
 		return false;
 	}
 
 	// ********************************************************//
 	// Attribute Functions //
 	// ********************************************************//
+	
 	public boolean createAttribute(String targetclass, String attribute) {
 		Entity e;
 		for (int index = 0; index < entities.size(); ++index) {
@@ -92,6 +102,27 @@ public class Classes {
 		// Target not found.
 		return false;
 	}
+
+	// ********************************************************//
+	// Relationship Functions //
+	// ********************************************************//
+
+	public boolean createRelationship(String name, String class1, String class2) {
+		Relationship r = new Relationship(name, class1, class2);
+		if (relationships.contains(r)) {
+			return false;
+		}
+		return relationships.add(r);
+	}
+
+	public boolean deleteRelationship(String name, String class1, String class2) {
+		Relationship r = new Relationship(name, class1, class2);
+		return relationships.remove(r);
+	}
+
+	// ********************************************************//
+	// Member Functions //
+	// ********************************************************//
 
 	public void clear() {
 		entities.clear();
