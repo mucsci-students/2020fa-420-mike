f82227d9b8280bf9e5e44c77beadcca4142672cf
SerbianSensation <56570006+SerbianSensation@users.noreply.github.com>
2020-09-18 17:38:33 -0400

    Update EntityTest.java

--- src/testcases/EntityTest.java
+++ src/testcases/EntityTest.java
@@ -142,11 +142,6 @@ public class EntityTest {
         assertEquals("List size is 1", 1, e.getMethods().size());
         assertTrue("a2 method still exists", e.getMethods().contains("a2"));
     }
-<<<<<<< HEAD
 
 }
 
-=======
-  
-}
->>>>>>> 96fe9f418ec7f2bf7b7e82d7702b75a1fdf64485
