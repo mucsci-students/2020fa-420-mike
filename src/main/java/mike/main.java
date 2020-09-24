7cc1f05df660c17c64f3c7bcb412ba33cba39bdc
phila413 <70169587+phila413@users.noreply.github.com>
2020-09-24 14:34:32 -0400

    Adds throws IOException

--- src/main/java/mike/main.java
+++ src/main/java/mike/main.java
@@ -1,10 +1,11 @@
 package mike;
 
+import java.io.IOException;
 import mike.cli.CommandLine;
 
 public class main {
 
-        public static void main(String[] args) {
+        public static void main(String[] args) throws IOException {
             CommandLine.commandInterface();
         }
 
