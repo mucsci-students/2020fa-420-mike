dfb14e8f2ef3e151f7d48f116ef72d91d4367aea
HomelessSkittle <conorsosh@gmail.com>
2020-10-06 20:20:24 -0400

    Small changes to improve clarity
    Removed unnecessary parameter to createClass in Controller.java, and changed some == equality checks to .equals in guiHelperMethods.java

--- src/main/java/mike/gui/Controller.java
+++ src/main/java/mike/gui/Controller.java
@@ -1,5 +1,6 @@
 package mike.gui;
 
+import javax.management.RuntimeErrorException;
 import javax.swing.*;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
@@ -17,10 +18,10 @@ import mike.gui.guiHelperMethods;
 
 public class Controller extends guiHelperMethods {
 
-	private static Classes userClasses;
+	private static Classes classes;
 
-	public Controller (Classes userClasses){
-		Controller.userClasses = userClasses;
+	public Controller (Classes classes){
+		this.classes = classes;
 	}
    
 	// Listen to any function calls
@@ -31,7 +32,7 @@ public class Controller extends guiHelperMethods {
 		  {
 			  String directory = System.getProperty("user.dir");
 			  try {
-				  save("uml.json", directory, userClasses);
+				  save("uml.json", directory, classes);
 			  }  catch (IOException e1) {
 				  e1.printStackTrace();
 			  }
@@ -46,7 +47,7 @@ public class Controller extends guiHelperMethods {
 		  {
 			  String directory = System.getProperty("user.dir");
 			  try {	
-				  save("uml.json", directory, userClasses);
+				  save("uml.json", directory, classes);
 			  }  catch (IOException e1) {
 				  e1.printStackTrace();
 			  }
@@ -63,7 +64,7 @@ public class Controller extends guiHelperMethods {
 			  String directory = System.getProperty("user.dir");
 			  try {
 				  directory += "\\uml.json";
-				   load(directory, userClasses);
+				   load(directory, classes);
 			  }  catch (Exception e1) {
 				  e1.printStackTrace();
 			  }
@@ -75,56 +76,61 @@ public class Controller extends guiHelperMethods {
 	public void treeListener(JTree tree, JFrame frame, HashMap<String, JLabel> entityLabels, JPanel centerPanel) {
 		tree.addTreeSelectionListener(new TreeSelectionListener() {
 			public void valueChanged(TreeSelectionEvent e) {
-				functionCalled(tree, userClasses, frame, entityLabels, centerPanel);
+				DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+				
+				/* if nothing is selected */ 
+				if (node == null) {
+					return;
+				}
+				
+				tree.clearSelection();
+				
+				switch(node.toString())
+				{
+					case "Class" : return;
+					case "Relationship" : return;
+					case "Field" : return;
+					case "Method" : return;
+					case "Parameter" : return;
+					case "list" : return;
+					default: break;
+				}
+				
+				// Create Drop-down box of existing classes
+				ArrayList<Entity> entities = classes.getEntities();
+				String[] entityStrings = new String[entities.size()];
+				for(int x = 0; x < entities.size(); ++x) {
+					entityStrings[x] = entities.get(x).getName();
+				}
+				JComboBox<String> list = new JComboBox<>(entityStrings);
+				
+				
+				/* React to the node selection. */
+				switch(node.toString()) {
+					case "Create Class" : createClass(classes); break;
+					case "Rename Class" : renameClass(classes, list); break;
+					case "Delete Class" : deleteClass(classes, list); break;
+					case "Create Relationship" : createOrDeleteRelation(classes, list, node.toString(), entityStrings); break;
+					case "Delete Relationship" : createOrDeleteRelation(classes, list, node.toString(), entityStrings); break;
+					case "Create Field" : createFieldsOrMethods(classes, list, "Field"); break;
+					case "Rename Field" : renameFieldsOrMethods(classes, list, frame, "Field"); break;
+					case "Delete Field" : deleteFieldsOrMethods(classes, list, frame, "Field"); break;
+					case "Create Method" : createFieldsOrMethods(classes, list, "Method"); break;
+					case "Rename Method" : renameFieldsOrMethods(classes, list, frame, "Method"); break;
+					case "Delete Method" : deleteFieldsOrMethods(classes, list, frame, "Method"); break;
+					case "Create Parameter" : createParameter(classes, list, frame); break;
+					case "Rename Parameter" : renameParameter(classes, list, frame); break;
+					case "Delete Parameter" : deleteParameter(classes, list, frame); break;
+					case "list all" :        	
+							System.out.println();							
+							listClasses(classes);
+							System.out.println();
+							listRelationships(classes);
+							System.out.println();
+							break;
+					default : throw new RuntimeException("Unknown button pressed");
+				}
 			}
 		});
 	}
-	
-	public static void functionCalled(JTree tree, Classes classes, JFrame frame, HashMap<String, JLabel> entityLabels, JPanel centerPanel) {
-		DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
-		
-		/* if nothing is selected */ 
-		if (node == null) {
-			return;
-		}
-		
-		tree.clearSelection();
-		// Create Drop-down box of existing classes
-		ArrayList<Entity> entities = classes.getEntities();
-		String[] entityStrings = new String[entities.size()];
-		for(int x = 0; x < entities.size(); ++x) {
-			entityStrings[x] = entities.get(x).getName();
-		}
-		JComboBox<String> list = new JComboBox<>(entityStrings);
-		
-		
-		/* React to the node selection. */
-		switch(node.toString()) {
-			case "Create Class" : createClass(classes, userClasses); break;
-			case "Rename Class" : renameClass(classes, list); break;
-			case "Delete Class" : deleteClass(classes, list); break;
-			case "Create Relationship" : createOrDeleteRelation(classes, list, node.toString(), entityStrings); break;
-			case "Delete Relationship" : createOrDeleteRelation(classes, list, node.toString(), entityStrings); break;
-			case "Create Field" : createFieldsOrMethods(classes, list, "Field"); break;
-			case "Rename Field" : renameFieldsOrMethods(classes, list, frame, "Field"); break;
-			case "Delete Field" : deleteFieldsOrMethods(classes, list, frame, "Field"); break;
-			case "Create Method" : createFieldsOrMethods(classes, list, "Method"); break;
-			case "Rename Method" : renameFieldsOrMethods(classes, list, frame, "Method"); break;
-			case "Delete Method" : deleteFieldsOrMethods(classes, list, frame, "Method"); break;
-			case "Create Parameter" : createParameter(classes, list, frame); break;
-			case "Rename Parameter" : renameParameter(classes, list, frame); break;
-			case "Delete Parameter" : deleteParameter(classes, list, frame); break;
-			default : 
-		}
-   
-		if (node.toString() == "list all"){        	
-			System.out.println();							
-			listClasses(classes);
-			System.out.println();
-			listRelationships(classes);
-			System.out.println();
-		}
-		
-	}
- 
 }
