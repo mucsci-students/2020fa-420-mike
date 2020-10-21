09fbc8cf90cca587978bc4c1b3d0ff8270da003e
JohnHynes <johnthehynes@gmail.com>
2020-10-21 13:43:51 -0400

    Implemented self-relationship visuals Split line drawing functions into drawSelfRelationship and drawRelationship

--- src/main/java/mike/gui/Line.java
+++ src/main/java/mike/gui/Line.java
@@ -8,8 +8,8 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.RenderingHints;
 import java.awt.Stroke;
+import java.awt.geom.Arc2D;
 import java.awt.geom.Line2D;
-import java.awt.geom.Path2D;
 
 import javax.swing.JComponent;
 import javax.swing.JLabel;
@@ -20,6 +20,7 @@ public class Line extends JComponent {
 	private static final long serialVersionUID = -5573668863596984829L;
 	private double x1, y1, x2, y2;
 	private JLabel L1, L2;
+	boolean isSelf;
 	private Type type;
 
 	public Line(JLabel L1, JLabel L2, Type type) {
@@ -27,6 +28,7 @@ public class Line extends JComponent {
 		this.type = type;
 		this.L1 = L1;
 		this.L2 = L2;
+		this.isSelf = L1.equals(L2);
 		update();
 
 		this.setVisible(true);
@@ -42,6 +44,14 @@ public class Line extends JComponent {
 	}
 
 	public void update() {
+		if (isSelf)
+		{
+			x1 = L1.getLocation().x;
+			y1 = L1.getLocation().y;
+			x2 = x1;
+			y2 = y1;
+			return;
+		}
 		// (a,b) = L1 center
 		double a = L1.getLocation().x + L1.getSize().width / 2;
 		double b = L1.getLocation().y + L1.getSize().height / 2;
@@ -96,6 +106,64 @@ public class Line extends JComponent {
 
 		super.paintComponent(g);
 
+		if (isSelf)
+		{
+			drawSelfRelationship(g2d);
+		}
+		else {
+			drawRelationship(g2d);
+		}
+	}
+	
+	private void drawSelfRelationship(Graphics2D g2d)
+	{
+		g2d.setColor(Color.BLACK);
+		
+		if (type == Type.REALIZATION)
+		{
+			float[] dashedpattern = { 4f, 4f };
+			Stroke dashedstroke = new BasicStroke(1f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 1.0f, dashedpattern,
+					1.0f);
+
+			g2d.setStroke(dashedstroke);
+			g2d.draw(new Arc2D.Double(x1 - 29, y1 - 29, 50, 50, 0, 240, Arc2D.OPEN));
+			
+			int x = (int)(x1 - 4);
+			int y = (int)(y1 + 16);
+			g2d.setStroke(new BasicStroke(1f));
+			g2d.rotate(-Math.toRadians(85), x, y);
+			drawEmptyArrow(g2d, x, y);
+			return;
+		}
+		
+		g2d.setStroke(new BasicStroke(1f));
+		g2d.draw(new Arc2D.Double(x1 - 29, y1 - 29, 50, 50, 0, 240, Arc2D.OPEN));
+		
+		if (type == Type.COMPOSITION)
+		{
+			int x = (int)(x1 - 22);
+			int y = (int)(y1 + 14);
+			
+			g2d.rotate(Math.toRadians(95), x, y);
+			drawFilledDiamond(g2d, x, y);
+		} else if (type == Type.AGGREGATION) {
+			int x = (int)(x1 - 22);
+			int y = (int)(y1 + 14);
+			
+			g2d.rotate(Math.toRadians(95), x, y);
+			drawEmptyDiamond(g2d, x, y);
+		}
+		else 
+		{
+			int x = (int)(x1 - 4);
+			int y = (int)(y1 + 16);
+			g2d.rotate(-Math.toRadians(85), x, y);
+			drawEmptyArrow(g2d, x, y);
+		}
+	}
+	
+	private void drawRelationship(Graphics2D g2d)
+	{
 		// Get line length
 		double ydiff = y2 - y1;
 		double xdiff = x2 - x1;
