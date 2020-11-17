1e4293f035bcf3786eb0abde1967e16b34cf5a59
Conor S <70168298+HomelessSkittle@users.noreply.github.com>
2020-11-17 15:31:24 -0500

    Removed GUIControllerTest
    Attempted to test a GUI thing, which is a big no-no

--- src/test/java/mike/controller/ControllerTest.java
+++ src/test/java/mike/controller/ControllerTest.java
@@ -11,15 +11,6 @@ import mike.view.ViewTemplate;
 import mike.view.ViewTemplate.InterfaceType;
 
 public class ControllerTest {
-
-    @Test
-    public void GUIControllerCreationTest() throws IOException {
-	Model m = new Model();
-	ViewTemplate v = new ViewTemplate(InterfaceType.GUI);
-	Controller c = new Controller(m, v);
-	
-	assertTrue("Controller equals null.", c != null);
-    }
     
     @Test
     public void CLIControllerCreationTest() throws IOException {
