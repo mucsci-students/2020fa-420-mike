b33eb6f914a2df22342638fc8fa3859d0ba6bc8a
SerbianSensation <stefangligorevic@gmail.com>
2020-12-07 16:12:35 -0500

    Keeps track of the current file being worked on in the CLI, calling save will now automatically save to the current file if there is one

--- src/main/java/mike/cli/LoadCommand.java
+++ src/main/java/mike/cli/LoadCommand.java
@@ -12,57 +12,63 @@ import mike.view.CLIView;
 public class LoadCommand extends CommandObj {
 
     private static LineReader savePromptReader;
+    private File file;
 
-    public LoadCommand(Model m, CLIView v, String[] com, boolean p, LineReader reader) {
-	super(m, v, com, p);
-	savePromptReader = reader;
+    public LoadCommand(Model m, CLIView v, String[] com, boolean p, LineReader reader, File file) {
+        super(m, v, com, p);
+        savePromptReader = reader;
+        this.file = file;
     }
 
     public boolean execute() {
-	if (commands.length == 2) {
-	    try {
-		if (prompt) {
-		    System.out.println("\nYou have unsaved changes, are you sure you want to continue?");
-		    System.out.println("Type 'yes' to continue loading, or 'no' to go back.");
-		    prompt = savePrompt();
-		}
-		if (!prompt) {
-		    File file = new File(commands[1]);
-		    HelperMethods.load(file, model, null, null);
-		    return false;
-		}
-	    } catch (Exception e) {
-		view.printError("Failed to parse directory. Exiting.\n");
-		return prompt;
-	    }
-	} else {
-	    view.printError(errorMessage + commandUsage[1] + "\n");
-	    return prompt;
-	}
-	return prompt;
+        if (commands.length == 2) {
+            try {
+                if (prompt) {
+                    System.out.println("\nYou have unsaved changes, are you sure you want to continue?");
+                    System.out.println("Type 'yes' to continue loading, or 'no' to go back.");
+                    prompt = savePrompt();
+                }
+                if (!prompt) {
+                    file = new File(commands[1]);
+                    HelperMethods.load(file, model, null, null);
+                    return false;
+                }
+            } catch (Exception e) {
+                view.printError("Failed to parse directory. Exiting.\n");
+                return prompt;
+            }
+        } else {
+            view.printError(errorMessage + commandUsage[1] + "\n");
+            return prompt;
+        }
+        return prompt;
     }
 
     // Gets user input to set save prompt flag.
     // False if they wish to continue
     // True if they want to return
     private static boolean savePrompt() {
-	while (prompt) {
+        while (prompt) {
 
-	    String line = savePromptReader.readLine("", "", (MaskingCallback) null, null);
-	    line = line.trim();
+            String line = savePromptReader.readLine("", "", (MaskingCallback) null, null);
+            line = line.trim();
 
-	    if (line.equals("yes")) {
-		System.out.println("Proceeding.\n");
-		prompt = false;
-		break;
-	    } else if (line.equals("no")) {
-		System.out.println("Stopping.\n");
-		prompt = true;
-		break;
-	    }
-	    System.out.println("Invalid command. Type 'yes' to proceed, or 'no' to go back.");
-	}
-	return prompt;
+            if (line.equals("yes")) {
+                System.out.println("Proceeding.\n");
+                prompt = false;
+                break;
+            } else if (line.equals("no")) {
+                System.out.println("Stopping.\n");
+                prompt = true;
+                break;
+            }
+            System.out.println("Invalid command. Type 'yes' to proceed, or 'no' to go back.");
+        }
+        return prompt;
     }
+
+    public File getFile() {
+    	return file;
+	}
 
 }
--- src/main/java/mike/cli/SaveCommand.java
+++ src/main/java/mike/cli/SaveCommand.java
@@ -9,25 +9,46 @@ import mike.view.CLIView;
 
 public class SaveCommand extends CommandObj {
 
-    public SaveCommand(Model m, CLIView v, String[] com, boolean p) {
-	super(m, v, com, p);
+    private File file;
+
+    public SaveCommand(Model m, CLIView v, String[] com, boolean p, File file) {
+        super(m, v, com, p);
+        this.file = file;
     }
 
     public boolean execute() {
-	if (commands.length == 2) {
-	    try {
-		File file = new File(commands[1]);
-		HelperMethods.save(file, model);
-		System.out.println("File saved at: " + file.getAbsolutePath());
-	    } catch (IOException e) {
-		view.printError("Failed to parse directory. Exiting.");
-		return prompt;
-	    }
-	} else {
-	    view.printError(errorMessage + commandUsage[0] + "\n");
-	    return prompt;
-	}
-	return false;
+        if (commands.length == 2) {
+        	//save to new specified file path
+            try {
+                file = new File(commands[1]);
+                HelperMethods.save(file, model);
+                System.out.println("File saved at: " + file.getAbsolutePath());
+            } catch (IOException e) {
+                view.printError("Failed to parse directory. Exiting.");
+                return prompt;
+            }
+        } else if(commands.length == 1) {
+        	if(file == null) {
+        		System.out.println("Specify a file path to save to. Proper command usage is: " + commandUsage[0] + "\n");
+        		return prompt;
+			}
+        	//save to current file
+			try {
+				HelperMethods.save(file, model);
+				System.out.println("File saved at: " + file.getAbsolutePath());
+			} catch (IOException e) {
+				view.printError("Failed to parse directory. Exiting.");
+				return prompt;
+			}
+		} else {
+            view.printError(errorMessage + commandUsage[0] + "\n");
+            return prompt;
+        }
+        return false;
+    }
+
+    public File getFile() {
+        return file;
     }
 
 }
